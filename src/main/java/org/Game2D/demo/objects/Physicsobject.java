97c49d30c4de67f2c96ef672f25f5062f713b266
acrazybirdy <silas.y.vogel@web.de>
2024-08-24 12:28:06 +0200

    making physicsobject tps dependant

--- src/main/java/org/Game2D/demo/objects/Physicsobject.java
+++ src/main/java/org/Game2D/demo/objects/Physicsobject.java
@@ -1,7 +1,6 @@
 package org.Game2D.demo.objects;
 
 import org.Game2D.engine.core.handlers.DataHand;
-import org.Game2D.engine.core.managers.ActionMan;
 import org.Game2D.engine.objects.advanced.Entity;
 
 import java.awt.*;
@@ -14,10 +13,10 @@ public class Physicsobject extends Entity {
     double passedTime;
 
     double gravityConst = 9.81;
-    double dragConst = 0.01;
-    double boost = 0.1;
+    double dragConst = 0.0;
+    double boost = 10;
 
-    double speedLooseAtCollisionPercent = 0.1;
+    double speedLossAtCollisionPercent = 1.25;
 
     public Physicsobject(boolean collision, Image txt, int tps) {
         super(new Rectangle(100,100,48,48), collision, txt);
@@ -28,22 +27,22 @@ public class Physicsobject extends Entity {
     @Override
     public void update() {
 
-        //If W or S is pressed there are jumpboosts
+        //If W or S is pressed there are jump-boosts
         if(DataHand.keyHand.keyPressed_W){
-            this.veloY -= this.boost;
+            this.veloY -= this.boost * this.passedTime;
         }
         if(DataHand.keyHand.keyPressed_S){
-            this.veloY += this.boost;
+            this.veloY += this.boost * this.passedTime;
         }
 
 
         //The movement on the x-axis is not accelerated
         //If A or D is pressed there is a speed boost
         if(DataHand.keyHand.keyPressed_A){
-            this.veloX -= this.boost;
+            this.veloX -= this.boost * this.passedTime;
         }
         if(DataHand.keyHand.keyPressed_D){
-            this.veloX += this.boost;
+            this.veloX += this.boost * this.passedTime;
         }
 
         boolean acceleration = true;
@@ -57,8 +56,9 @@ public class Physicsobject extends Entity {
             this.veloX = this.veloX*-1;
             moveX *= -1;
 
-            //if the vriable for speed loose at collision is set the object looses a given percentage of its speed
-            this.veloX *= 1-this.speedLooseAtCollisionPercent;
+
+            //if the variable for speed loose at collision is set the object looses a given percentage of its speed
+            this.veloX *= 1-(this.speedLossAtCollisionPercent * this.passedTime);
         }
 
         //Bottom and Top logic -> if it hits bottom or top it goes the opposite direction
@@ -67,8 +67,8 @@ public class Physicsobject extends Entity {
             moveY *= -1;
             acceleration = false;
 
-            //if the vriable for speed loose at collision is set the object looses a given percentage of its speed
-            this.veloY *= 1-this.speedLooseAtCollisionPercent;
+            //if the variable for speed loose at collision is set the object looses a given percentage of its speed
+            this.veloY *= 1-(this.speedLossAtCollisionPercent * this.passedTime);
         }
 
 
@@ -86,13 +86,13 @@ public class Physicsobject extends Entity {
         //the drag constant is just a number that actively works against the direction in which the object moves
         //if the subtraction of the drag constant would accelerate the object in the opposite direction the speed is set to 0
         //in X direction
-        if((this.dragConst*-1) < veloX && this.dragConst > veloX){ veloX = 0;}
-        if(veloX < 0){ veloX += this.dragConst; }
-        if(veloX > 0){ veloX -= this.dragConst; }
+        if((this.dragConst*-1*this.passedTime) < veloX && this.dragConst * this.passedTime > veloX){ veloX = 0;}
+        if(veloX < 0){ veloX += this.dragConst * this.passedTime; }
+        if(veloX > 0){ veloX -= this.dragConst * this.passedTime; }
         //in Y direction
-        if((this.dragConst*-1) < veloY && this.dragConst > veloY){ veloY = 0;}
-        if(veloY < 0){ veloY += this.dragConst; }
-        if(veloY > 0){ veloY -= this.dragConst; }
+        if((this.dragConst*-1*this.passedTime) < veloY && this.dragConst * this.passedTime > veloY){ veloY = 0;}
+        if(veloY < 0){ veloY += this.dragConst * this.passedTime; }
+        if(veloY > 0){ veloY -= this.dragConst * this.passedTime; }
 
 
         //moving the object at last
