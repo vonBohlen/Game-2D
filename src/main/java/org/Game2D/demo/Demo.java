a580a143cd1649d34bf418cad558b0d48080101d
vonBohlen <Christian.vonBohlen@gmail.com>
2024-09-26 22:47:58 +0200

    s

--- src/main/java/org/Game2D/demo/Demo.java
+++ src/main/java/org/Game2D/demo/Demo.java
@@ -1,6 +1,10 @@
 package org.Game2D.demo;
 
 import org.Game2D.demo.controlable.Physicsobject;
+import org.Game2D.demo.controlable.Player;
+import org.Game2D.demo.entities.Ball;
+import org.Game2D.demo.entities.BallwP;
+import org.Game2D.demo.entities.TpSquare;
 import org.Game2D.demo.flappy.entities.Bird;
 import org.Game2D.demo.objects.Square;
 import org.Game2D.engine.core.Instance;
@@ -26,19 +30,17 @@ public class Demo {
             throw new RuntimeException(e);
         }
 
-        //new Player(new Rectangle(200, 200, 48, 48), texture);
+        new Player(new Rectangle(200, 200, 48, 48), texture);
 
-        //new Square(new Rectangle(100, 52, 48, 48), texture);
+        new Square(new Rectangle(100, 52, 48, 48), texture);
 
-        //new BallwP(new Rectangle(600, 0, 48, 48), texture);
+        new BallwP(new Rectangle(600, 0, 48, 48), texture);
 
-        //new TpSquare(new Rectangle(800, 800, 48, 48), texture);
+        new TpSquare(new Rectangle(800, 800, 48, 48), texture);
 
-        //new Ball(new Rectangle(0, 0, 48, 48), texture);
+        new Ball(new Rectangle(0, 0, 48, 48), texture);
 
-        //new Physicsobject(true, texture, 60);
-
-        //new Bird(texture);
+        new Physicsobject(true, texture, 60);
 
     }
 
