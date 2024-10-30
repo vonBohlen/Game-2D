696798ce8237e5ba2d98ee589316dbf41ff8308d
vonBohlen <Christian.vonBohlen@gmail.com>
2024-10-30 22:48:27 +0100

    Modified DebugScreen

--- src/main/java/org/Game2D/engine/utils/DebugScreen.java
+++ src/main/java/org/Game2D/engine/utils/DebugScreen.java
@@ -3,6 +3,7 @@ package org.Game2D.engine.utils;
 import org.Game2D.engine.core.handlers.DataHand;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class DebugScreen {
 
@@ -20,12 +21,14 @@ public class DebugScreen {
     }
 
     public static void draw(Graphics2D g2) {
-        if (/*Boolean.parseBoolean(ConfProvider.getConf(DataHand.confPath).getProperty("game2d.core.showDebugScreen"))*/ true) {
+        if (Boolean.parseBoolean(Objects.requireNonNull(ConfProvider.getConf(DataHand.confPath)).getProperty("game2d.core.showDebugScreen"))) {
             g2.setColor(Color.RED);
 
             g2.drawString("FPS: " + FPS, 20, 20);
 
             g2.drawString("TPS: " + TPS, 20, 30);
+
+            g2.drawString("Objects:" + DataHand.getGameObjs().size(), 20, 50);
         }
     }
 
