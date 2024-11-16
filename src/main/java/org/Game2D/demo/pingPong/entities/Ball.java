4badc0bc56a5fe846de245fc7f16dc28c0b1c52e
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-16 18:37:46 +0530

    Created the ball

--- src/main/java/org/Game2D/demo/pingPong/entities/Ball.java
+++ src/main/java/org/Game2D/demo/pingPong/entities/Ball.java
@@ -1,4 +1,25 @@
 package org.Game2D.demo.pingPong.entities;
 
-public class Ball {
+import org.Game2D.demo.pingPong.PingPong;
+import org.Game2D.engine.core.handlers.DataHand;
+import org.Game2D.engine.objects.advanced.Entity;
+
+import java.awt.*;
+
+public class Ball extends Entity {
+
+    public Ball(boolean collision, Image txt) {
+        super(new Rectangle(0,0, PingPong.screenWidth / 50, PingPong.screenWidth / 50), collision, txt);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.WHITE);
+        g2.drawImage(texture, hitBox.x, hitBox.y, hitBox.width, hitBox.height, null);
+    }
 }
