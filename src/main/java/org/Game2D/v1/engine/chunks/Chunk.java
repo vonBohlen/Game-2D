e8186e0e49ee424307c67a1fe6b88ac328ecf889
vonBohlen <Christian.vonBohlen@gmail.com>
2025-02-01 18:47:43 +0100

    Chunks

--- src/main/java/org/Game2D/v1/engine/chunks/Chunk.java
+++ src/main/java/org/Game2D/v1/engine/chunks/Chunk.java
@@ -2,6 +2,7 @@ package org.Game2D.v1.engine.chunks;
 
 import org.Game2D.v1.engine.objects.GameObject;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
@@ -10,14 +11,12 @@ public class Chunk {
 
     private volatile boolean lockAcquired = false;
 
-    private Vector<Integer> id = new Vector<>(3);
+    private final Vector<Integer> id;
 
-    private final List<GameObject> objects = new ArrayList<>();
+    private final List<GameObject> gameObjects = new ArrayList<>();
 
-    public Chunk(int chunkX, int chunkY, int size, List<GameObject> initialObjects) {
-        id.add(0, chunkX);
-        id.add(1, chunkY);
-        id.add(2, size);
+    public Chunk(Vector<Integer> id, List<GameObject> initialObjects) {
+        this.id = id;
 
         addGameObject(initialObjects);
     }
@@ -35,32 +34,52 @@ public class Chunk {
 
     public void addGameObject(GameObject gameObject) {
         acquirerLock();
-        objects.add(gameObject);
+        gameObjects.add(gameObject);
         dropLock();
     }
 
     public void addGameObject(List<GameObject> gameObjects) {
         acquirerLock();
-        objects.addAll(gameObjects);
+        this.gameObjects.addAll(gameObjects);
         dropLock();
     }
 
     public void removeGameObject(GameObject gameObject) {
         acquirerLock();
-        objects.removeIf(current -> current == gameObject);
+        gameObjects.removeIf(current -> current == gameObject);
+        dropLock();
+    }
+
+    public void removeGameObjects(List<GameObject> gameObjects) {
+        acquirerLock();
+        this.gameObjects.removeAll(gameObjects);
         dropLock();
     }
 
     public void update() {
         acquirerLock();
-        for (GameObject gameObject : objects) {
+        for (GameObject gameObject : gameObjects) {
             gameObject.update();
         }
         dropLock();
     }
 
-//    public List<GameObject> getGameObjs() {
-//        return (List<GameObject>) objects.clone();
+    public void draw(Graphics2D g2) {
+        acquirerLock();
+        for (GameObject gameObject : gameObjects) {
+            gameObject.draw(g2);
+        }
+        dropLock();
+    }
+
+    //TODO: Gamobjects Liste so returnen, dass die GameObjjects Liste des Chunks nicht manipolierbar ist.
+//    public List<GameObject> getGameObjects() {
+//        return (List<GameObject>) gameObjects.clone();
 //    }
+
+    public Vector<Integer> getId() {
+        //TODO: Id Vector so returnen, dass der Id Vector des Chunks nicht manipolierbar ist.
+        return new Vector<>();
+    }
 
 }
