4eb1b1e46085dae61b94d5552f4233cf6799224a
vonBohlen <Christian.vonBohlen@gmail.com>
2025-02-02 02:11:44 +0100

    Chunk

--- src/main/java/org/Game2D/v1/engine/chunks/ChunkFactory.java
+++ src/main/java/org/Game2D/v1/engine/chunks/ChunkFactory.java
@@ -7,8 +7,11 @@ import java.util.Vector;
 
 public class ChunkFactory {
 
-    public static Chunk generateChunk(int posX, int posY, List<Integer> ids, List<GameObject> gameObjects) {
-        Vector<Integer> id = new Vector<>(posX, posY);
+    public static Chunk generateChunk(int posX, int posY, int size, List<Integer> ids, List<GameObject> gameObjects) {
+        Vector<Integer> id = new Vector<>();
+        id.add(posX);
+        id.add(posY);
+        id.add(size);
         id.addAll(ids);
         return new Chunk(id, gameObjects);
     }
