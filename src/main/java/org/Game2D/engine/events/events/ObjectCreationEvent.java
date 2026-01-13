79b79da6946bd1164bf29a2bcf14a7f7322b0321
vonBohlen <Christian.vonBohlen@gmail.com>
2026-01-13 01:50:06 +0100

    Added prototype for event system

---
+++ src/main/java/org/Game2D/engine/events/events/ObjectCreationEvent.java
@@ -0,0 +1,34 @@
+package org.Game2D.engine.events.events;
+
+import lombok.Getter;
+import org.Game2D.engine.events.handler.interfaces.ObjectCreationHandler;
+import org.Game2D.engine.objects.GameObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ObjectCreationEvent {
+
+    @Getter
+    private static final List<ObjectCreationHandler> handlers = Collections.synchronizedList(new ArrayList<>());
+
+    public static void addListener(ObjectCreationHandler listener) {
+        synchronized (handlers) {
+            handlers.add(listener);
+        }
+    }
+
+    public static void removeListener(ObjectCreationHandler listener) {
+        synchronized (handlers) {
+            handlers.remove(listener);
+        }
+    }
+
+    public static void callEvent(GameObject gameObject) {
+        synchronized (handlers) {
+            handlers.forEach(handler -> handler.handelObjectCreationEvent(gameObject));
+        }
+    }
+
+}
---
+++ src/main/java/org/Game2D/engine/events/handler/interfaces/ObjectCreationHandler.java
@@ -0,0 +1,9 @@
+package org.Game2D.engine.events.handler.interfaces;
+
+import org.Game2D.engine.objects.GameObject;
+
+public interface ObjectCreationHandler {
+
+    void handelObjectCreationEvent(GameObject object);
+
+}
