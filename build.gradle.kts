26acb3e8d9e3720cec65b4926f748bada3ac7cde
Justus Kagerer <justus-kagerer@web.de>
2026-02-22 10:16:31 +0100

    Fix LWJGL deps in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ plugins {
 group = "org.Game2D"
 version = "1.0.0"
 
+val lwjglVersion = "3.4.1"
+val lwjglNatives = "natives-linux"
+
 java {
 
     sourceCompatibility = JavaVersion.VERSION_17
@@ -29,8 +32,26 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("org.reflections:reflections:0.10.2")
 
-    implementation("org.lwjgl:lwjgl:3.4.1")
-    implementation("org.lwjgl:lwjgl-glfw:3.4.1")
-    implementation("org.lwjgl:lwjgl-opengl:3.4.1")
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nanovg")
+    implementation("org.lwjgl", "lwjgl-nuklear")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-par")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-vulkan")
+    implementation ("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-nanovg", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-nuklear", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-par", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
 
 }
