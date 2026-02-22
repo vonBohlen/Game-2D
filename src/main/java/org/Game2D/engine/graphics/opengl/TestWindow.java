685590fd8bff391f1b9bf34e51d8e3d9d65a5b23
Justus Kagerer <justus-kagerer@web.de>
2026-02-22 11:05:28 +0100

    Basic PoC in GLFW

--- src/main/java/org/Game2D/engine/graphics/opengl/TestWindow.java
+++ src/main/java/org/Game2D/engine/graphics/opengl/TestWindow.java
@@ -1,22 +1,27 @@
 package org.Game2D.engine.graphics.opengl;
 
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
 
-import java.nio.*;
+import java.nio.IntBuffer;
 
-import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class TestWindow {
 
     // The window handle
     private long window;
+
+    public static void main(String[] args) {
+        new TestWindow().run();
+    }
 
     public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -34,13 +39,12 @@ public class TestWindow {
     }
 
     private void init() {
-        // Setup an error callback. The default implementation
+        // Set up an error callback. The default implementation
         // will print the error message in System.err.
         GLFWErrorCallback.createPrint(System.err).set();
 
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( !glfwInit() )
-            throw new IllegalStateException("Unable to initialize GLFW");
+        if (!glfwInit()) throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure GLFW
         glfwDefaultWindowHints(); // optional, the current window hints are already the default
@@ -48,18 +52,17 @@ public class TestWindow {
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
         // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
+        window = glfwCreateWindow(640, 320, "Hello World!", NULL, NULL);
+        if (window == NULL) throw new RuntimeException("Failed to create the GLFW window");
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
                 glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
         });
 
         // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
+        try (MemoryStack stack = stackPush()) {
             IntBuffer pWidth = stack.mallocInt(1); // int*
             IntBuffer pHeight = stack.mallocInt(1); // int*
 
@@ -70,11 +73,7 @@ public class TestWindow {
             GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
             // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
+            glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0)) / 2, (vidmode.height() - pHeight.get(0)) / 2);
         } // the stack frame is popped automatically
 
         // Make the OpenGL context current
@@ -94,13 +93,38 @@ public class TestWindow {
         // bindings available for use.
         GL.createCapabilities();
 
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0.0, 640, 320, 0.0, -1.0, 1.0);
+        glMatrixMode(GL_MODELVIEW);
+
+
         // Set the clear color
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 
+        float r = 0.75f, g = 0.25f, b = 0;
+        float dc = 0.01f;
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( !glfwWindowShouldClose(window) ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        while (!glfwWindowShouldClose(window)) {
+            if (r + dc > 1 || g + dc > 1) {
+                r = 0;
+                g = 0;
+            }
+            r += dc;
+            g += dc;
+            glClearColor(r, g, b, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);// clear the framebuffer
+
+            glBegin(GL_POLYGON);
+            glVertex2i(100, 100);
+            glColor3f(r,g,0.5f);
+            glVertex2i(100, 200);
+            glColor3f(1,0.5f,0);
+            glVertex2i(200, 200);
+            glVertex2i(200, 100);
+            glEnd();
+            glFlush();
 
             glfwSwapBuffers(window); // swap the color buffers
 
@@ -108,10 +132,6 @@ public class TestWindow {
             // invoked during this call.
             glfwPollEvents();
         }
-    }
-
-    public static void main(String[] args) {
-        new TestWindow().run();
     }
 
 }
