8780599e8d4428ecfbcf9cfca1c941a298241fbe
vonBohlen <Christian.vonBohlen@gmail.com>
2026-03-14 15:55:49 +0100

    Added comments to AssetManager

--- src/main/java/org/Game2D/engine/data/disk/assets/AssetManager.java
+++ src/main/java/org/Game2D/engine/data/disk/assets/AssetManager.java
@@ -1,12 +1,14 @@
 /**
  * /engine/data/disk/assets/AssetMan.java
  *
- * DESCRIPTION
+ * Utility class for managing image assets.
  *
  * Copyright (C) 2026 Christian von Bohlen
  */
 
 package org.Game2D.engine.data.disk.assets;
+
+import lombok.NonNull;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -15,11 +17,22 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Utility class for managing image assets.
+ */
 public class AssetManager {
 
+    /**
+     * Hashmap of all loaded assets.
+     */
     private static final ConcurrentHashMap<String, Image> loadedAssets = new ConcurrentHashMap<>();
 
-    public static void loadAsset(String path) {
+    /**
+     * Loads an asset from the resource folder into memory.
+     *
+     * @param path Path of the asset in the resource folder
+     */
+    public static void loadAsset(@NonNull String path) {
         if (loadedAssets.containsKey(path)) return;
         Image asset;
         try {
@@ -31,31 +44,65 @@ public class AssetManager {
         loadedAssets.put(path, asset);
     }
 
-    public static void loadAssets(String[] paths) {
+    /**
+     * Loads multiple assets from the resource folder into memory.
+     *
+     * @param paths Paths of the assets in the resource folder
+     */
+    public static void loadAssets(@NonNull String[] paths) {
         for (String path : paths) loadAsset(path);
     }
 
-    public static void unloadAsset(String path) {
+    /**
+     * Removes a loaded asset from memory.
+     *
+     * @param path Path of the asset in the resource folder
+     */
+    public static void unloadAsset(@NonNull String path) {
         loadedAssets.remove(path);
     }
 
-    public static void unloadAssets(String[] paths) {
+    /**
+     * Removes multiple loaded assets from memory.
+     *
+     * @param paths Paths of the assets in the resource folder
+     */
+    public static void unloadAssets(@NonNull String[] paths) {
         for (String path : paths) loadedAssets.remove(path);
     }
 
+    /**
+     * Get the fallback asset.
+     *
+     * @return Fallback asset
+     */
     public static Image getFallback() {
         loadAsset("default.png");
         if (loadedAssets.containsKey("default.png")) return loadedAssets.get("default.png");
         throw new RuntimeException("Error while loading asset");
     }
 
-    public static Image getAsset(String path) {
+    /**
+     * Get a loaded asset, loads asset if not already loaded.
+     *
+     * @param path Path of the asset in the resource folder
+     *
+     * @return Wanted asset
+     */
+    public static Image getAsset(@NonNull String path) {
         loadAsset(path);
         if (loadedAssets.containsKey(path)) return loadedAssets.get(path);
         return getFallback();
     }
 
-    public static ArrayList<Image> getAssets(String[] paths) {
+    /**
+     * Get multiple loaded assets, loads assets if not already loaded.
+     *
+     * @param paths Paths of the assets in the resource folder
+     *
+     * @return Wanted assets
+     */
+    public static ArrayList<Image> getAssets(@NonNull String[] paths) {
         ArrayList<Image> assets = new ArrayList<>();
         loadAssets(paths);
         for (int i = paths.length; i > 0; ) {
